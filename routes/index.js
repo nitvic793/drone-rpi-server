c07c6d75eadb10544cbde26b3deff9329f944d7e
nitvic793 <nithishvictor@gmail.com>
2015-10-19 15:35:34 +0530

    Drone package added.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var arDrone = require('ar-drone');
+var client  = arDrone.createClient();
 
-/* GET home page. */
+/* GET home page. Takes off and lands on  */
 router.get('/', function(req, res, next) {
+	client.takeoff();
+	client.after(2000, function() {
+		this.stop();
+		this.land();
+	});
+	console.log("Take off and land");	
   res.render('index', { title: 'Express' });
 });
+
+
 
 module.exports = router;
